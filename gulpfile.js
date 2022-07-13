4c9431cf776a2351f654ebe75534ed91fa7a4bcf
Evgeniy Vasilevskiy <snuveg@mail.ru>
2022-07-13 22:52:56 +1000

    1.1.2

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,10 @@ const autoprefixer = require('gulp-autoprefixer');
 // const px2rem = require('gulp-smile-px2rem'); //.pipe(px2rem())
 const gcmq = require('gulp-group-css-media-queries');
 const cleanCSS = require('gulp-clean-css');
-// const uglify = require('gulp-uglify');
+// const uglify = require('gulp-uglify');  ----not worked----
+const gulpif = require('gulp-if');
 
+const env = process.env.NODE_ENV;
 
 const styles = ['src/styles/normalize.css',
   'src/main.css']
@@ -18,7 +20,8 @@ function clean() {
 };
 function copyHtml() {
   return src('src/*.html')
-    .pipe(dest('dist'));
+    .pipe(gulpif(env == 'prod', dest('dist')))
+    .pipe(gulpif(env == 'dev', dest('test')));
 };
 function copyCss() {
   return src(styles)
@@ -26,17 +29,20 @@ function copyCss() {
     .pipe(autoprefixer({ cascade: false }))
     .pipe(gcmq())
     .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(dest('dist'));
+    .pipe(gulpif(env == 'prod', dest('dist')))
+    .pipe(gulpif(env == 'dev', dest('test')));
 };
 function copyJs() {
   return src('src/*.js')
     .pipe(concat('main.js', { newLine: ';' }))
     // .pipe(uglify)    ----not worked----
-    .pipe(dest('dist'));
+    .pipe(gulpif(env == 'prod', dest('dist')))
+    .pipe(gulpif(env == 'dev', dest('test')));
 };
 function copyImg() {
   return src('src/img/**/*')
-    .pipe(dest('dist/img'));
+  .pipe(gulpif(env == 'prod', dest('dist/img')))
+    .pipe(gulpif(env == 'dev', dest('test/img')));
 };
 
 exports.default = series(clean, parallel(copyHtml, copyCss, copyJs, copyImg));
