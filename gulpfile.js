96c33a5ed0aeb77d3e8b22a9108a967adb2031ea
Evgeniy Vasilevskiy <snuveg@mail.ru>
2022-07-13 22:20:27 +1000

    1.1.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,44 @@
-const { src, dest, series, watch } = require('gulp');
+const { src, dest, series, parallel, watch } = require('gulp');
 const rm = require('gulp-rm');
-const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+// const px2rem = require('gulp-smile-px2rem'); //.pipe(px2rem())
+const gcmq = require('gulp-group-css-media-queries');
+const cleanCSS = require('gulp-clean-css');
+// const uglify = require('gulp-uglify');
+
+
+const styles = ['src/styles/normalize.css',
+  'src/main.css']
+const scripts = ['src/jquery.touchSwipe.min.js',
+  'src/main.js']
 
 function clean() {
   return src('dist/**/*', { read: false }).pipe(rm());
 };
-function styling() {
-  return src('src/styles/main.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(dest('dist'));
+function copyHtml() {
+  return src('src/*.html')
+    .pipe(dest('dist'));
+};
+function copyCss() {
+  return src(styles)
+    .pipe(concat('main.css'))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(gcmq())
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(dest('dist'));
+};
+function copyJs() {
+  return src('src/*.js')
+    .pipe(concat('main.js', { newLine: ';' }))
+    // .pipe(uglify)    ----not worked----
+    .pipe(dest('dist'));
+};
+function copyImg() {
+  return src('src/img/**/*')
+    .pipe(dest('dist/img'));
 };
 
-exports.default = series(clean, styling);
+exports.default = series(clean, parallel(copyHtml, copyCss, copyJs, copyImg));
 
 // watch('./src/styles/main.scss', series(clean, styling));
